8c3de405f53c935b62d34f689b9ef764bfaa0582
leo10099 <leandrohanc@gmail.com>
2018-07-13 16:00:52 -0300

    img gallery ready

--- client/src/components/presentational/Gallery/PhotoGallery.js
+++ client/src/components/presentational/Gallery/PhotoGallery.js
@@ -6,10 +6,13 @@ import { Link } from 'react-router-dom'
 import Lightbox from 'react-images'
 import Measure from 'react-measure'
 import _ from 'lodash'
+import { RingLoader } from 'react-spinners'
+
 export default class PhotoGallery extends Component {
   state = {
     currentImage: 0,
-    width: -1
+    width: -1,
+    loading: true
   }
   constructor () {
     super()
@@ -58,51 +61,60 @@ export default class PhotoGallery extends Component {
       this.renameProp('secure_url', 'src', datita)
     )
     const width = this.state.width
-    return (
-      <Measure
-        bounds
-        onResize={contentRect =>
-          this.setState({ width: contentRect.bounds.width })}
-      >
-        {({ measureRef }) => {
-          if (width < 1) {
-            return <div ref={measureRef} />
-          }
-          let columns = 1
-          if (width >= 480) {
-            columns = 2
-          }
-          if (width >= 1024) {
-            columns = 3
-          }
-          return (
-            <div className='image-grid__container' ref={measureRef}>
-              <Nav
-                resetGallery={this.props.resetGallery}
-                currentGallery={this.props.currentGallery}
-              />
-              <ReactPhotoGallery
-                photos={fotos}
-                onClick={this.openLightbox}
-                columns={columns}
-              />
-              <Lightbox
-                images={fotos}
-                onClose={this.closeLightbox}
-                onClickPrev={this.gotoPrevious}
-                onClickNext={this.gotoNext}
-                currentImage={this.state.currentImage}
-                isOpen={this.state.lightboxIsOpen}
-              />
-              <Footer resetGallery={this.props.resetGallery}>
-                <i className='fa fa-caret-left' />
-                {' '}
-                <Link to='/galeria'>Volver</Link>
-              </Footer>
-            </div>
-          )
-        }}
-      </Measure>
-    )
+
+    if (this.state.loading === true) {
+      return (
+        <div className='sweet-loading'>
+          <RingLoader color={'#123abc'} loading={this.state.loading} />
+        </div>
+      )
+    } else {
+      return (
+        <Measure
+          bounds
+          onResize={contentRect =>
+            this.setState({ width: contentRect.bounds.width })}
+        >
+          {({ measureRef }) => {
+            if (width < 1) {
+              return <div ref={measureRef} />
+            }
+            let columns = 1
+            if (width >= 480) {
+              columns = 2
+            }
+            if (width >= 1024) {
+              columns = 3
+            }
+            return (
+              <div className='image-grid__container' ref={measureRef}>
+                <Nav
+                  resetGallery={this.props.resetGallery}
+                  currentGallery={this.props.currentGallery}
+                />
+                <ReactPhotoGallery
+                  photos={fotos}
+                  onClick={this.openLightbox}
+                  columns={columns}
+                />
+                <Lightbox
+                  images={fotos}
+                  onClose={this.closeLightbox}
+                  onClickPrev={this.gotoPrevious}
+                  onClickNext={this.gotoNext}
+                  currentImage={this.state.currentImage}
+                  isOpen={this.state.lightboxIsOpen}
+                />
+                <Footer resetGallery={this.props.resetGallery}>
+                  <i className='fa fa-caret-left' />
+                  {' '}
+                  <Link to='/galeria'>Volver</Link>
+                </Footer>
+              </div>
+            )
+          }}
+        </Measure>
+      )
+    }
   }
 }
