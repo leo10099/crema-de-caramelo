4890a87a9dc66fb79bb4f3f2b6b95a81478a3ca5
leo10099 <leandrohanc@gmail.com>
2018-07-14 17:32:36 -0300

    resize bug fixed

--- client/src/components/presentational/Gallery/PhotoGallery.js
+++ client/src/components/presentational/Gallery/PhotoGallery.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react'
 import Nav from '../Nav'
 import Footer from './Footer'
 import ReactPhotoGallery from 'react-photo-gallery'
-import { Link } from 'react-router-dom'
-import { GridLoader } from 'react-spinners'
 import Lightbox from 'react-images'
+import _ from 'lodash'
 import Measure from 'react-measure'
 
 export default class PhotoGallery extends Component {
@@ -14,17 +13,21 @@ export default class PhotoGallery extends Component {
   }
   constructor () {
     super()
+    this.state = { currentImage: 0 }
     this.closeLightbox = this.closeLightbox.bind(this)
     this.openLightbox = this.openLightbox.bind(this)
     this.gotoNext = this.gotoNext.bind(this)
     this.gotoPrevious = this.gotoPrevious.bind(this)
   }
-  componentDidMount () {
+  componentWillMount () {
     const { setCloudData, fetchTheme } = this.props
     setCloudData(fetchTheme)
-    this.forceUpdate()
   }
 
+  renameProp = (oldProp, newProp, { [oldProp]: old, ...others }) => ({
+    [newProp]: old,
+    ...others
+  })
   openLightbox (event, obj) {
     this.setState({
       currentImage: obj.index,
@@ -48,75 +51,55 @@ export default class PhotoGallery extends Component {
     })
   }
 
-  renameProp = (oldProp, newProp, { [oldProp]: old, ...others }) => ({
-    [newProp]: old,
-    ...others
-  })
-
   render () {
-    // Formatear los datos según las prop-types requeridas por React Photo Gallery
-    const data = [...this.props.cloudData].reduce((a, b) => a.concat(b), [])
-    const fotos = data.map(foto => this.renameProp('secure_url', 'src', foto))
+    const data = _.flatten(this.props.cloudData)
+    // Renombrar la URL segura de Cloudinary por src, prop-name requerida por react-photo-gallery
+    const fotos = data.map(datita =>
+      this.renameProp('secure_url', 'src', datita)
+    )
     const width = this.state.width
-    if (!this.props.cloudData[0]) {
-      return (
-        <section>
-          <Nav
-            resetGallery={this.props.resetGallery}
-            currentGallery={this.props.currentGallery}
-          />
-          <div className='spinner'>
-            <GridLoader color={'#665687'} loading={this.state.loading} />
-          </div>
-        </section>
-      )
-    } else {
-      return (
-        <Measure
-          bounds
-          onResize={contentRect =>
-            this.setState({ width: contentRect.bounds.width })}
-        >
-          {({ measureRef }) => {
-            if (width < 1) {
-              return <div ref={measureRef} />
-            }
-            let columns = 1
-            if (width >= 480) {
-              columns = 2
-            }
-            if (width >= 1024) {
-              columns = 3
-            }
-            return (
-              <div className='image-grid__container' ref={measureRef}>
-                <Nav
-                  resetGallery={this.props.resetGallery}
-                  currentGallery={this.props.currentGallery}
-                />
-                <ReactPhotoGallery
-                  photos={fotos}
-                  onClick={this.openLightbox}
-                  columns={columns}
-                />
-                <Lightbox
-                  images={fotos}
-                  onClose={this.closeLightbox}
-                  onClickPrev={this.gotoPrevious}
-                  onClickNext={this.gotoNext}
-                  currentImage={this.state.currentImage}
-                  isOpen={this.state.lightboxIsOpen}
-                />
-                <Footer resetGallery={this.props.resetGallery}>
-                  <i className='fa fa-caret-left' />
-                  {' '}
-                  <Link to='/galeria'>Volver</Link>
-                </Footer>
-              </div>
-            )
-          }}
-        </Measure>
-      )
-    }
+    return (
+      <Measure
+        bounds
+        onResize={contentRect =>
+          this.setState({ width: contentRect.bounds.width })}
+      >
+        {({ measureRef }) => {
+          if (width < 1) {
+            return <div ref={measureRef} />
+          }
+          let columns = 1
+          if (width >= 480) {
+            columns = 2
+          }
+          if (width >= 1024) {
+            columns = 3
+          }
+          return (
+            <div className='image-grid__container' ref={measureRef}>
+              <Nav />
+              <ReactPhotoGallery
+                photos={fotos}
+                onClick={this.openLightbox}
+                columns={columns}
+              />
+              <Lightbox
+                images={fotos}
+                onClose={this.closeLightbox}
+                onClickPrev={this.gotoPrevious}
+                onClickNext={this.gotoNext}
+                currentImage={this.state.currentImage}
+                isOpen={this.state.lightboxIsOpen}
+              />
+              <Footer resetGallery={this.props.resetGallery}>
+                <span className='fake-link'>
+                  <i className='fa fa-caret-left' /> {' '} Volver
+                </span>
+              </Footer>
+            </div>
+          )
+        }}
+      </Measure>
+    )
   }
 }
