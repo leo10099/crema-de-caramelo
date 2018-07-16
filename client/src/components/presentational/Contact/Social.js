b8bb2222ce600ec01b7723530883ab1026801a4d
leo10099 <leandrohanc@gmail.com>
2018-07-16 14:55:59 -0300

    contact done

--- client/src/components/presentational/Contact/Social.js
+++ client/src/components/presentational/Contact/Social.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react'
 import ContactButton from '../../ui-elements/contactButton'
-import { Link } from 'react-router-dom'
 
 export default class Social extends Component {
   goToFb () {
     window.open('https://www.facebook.com/cremadecaramelocandybar')
   }
+  sendWhatsapp () {
+    window.open('https://wa.me/5491162470115')
+  }
+
   render () {
     return (
       <section className='social-container'>
+        <div className='subtitulo--seccion'>
+          <i className='fa fa-phone-square' />{' '}Teléfono: 11 - 6247 - 0115
+        </div>
         <div className='contact__button-panel'>
+          <ContactButton className='mobile-only' onClick={this.sendWhatsapp}>
+            Mensajeanos por {' '}
+            <i className='fab fa-whatsapp' />{' '}
+          </ContactButton>
           <ContactButton onClick={this.goToFb}>
             Enterate más sobre nosotras en {' '}
             <i className='fab fa-facebook-f' />
-          </ContactButton>
-          <ContactButton>
-            Mensajeanos por {' '}
-            <i className='fab fa-whatsapp' />
           </ContactButton>
         </div>
       </section>
