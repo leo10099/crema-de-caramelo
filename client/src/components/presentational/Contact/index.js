d4dd85d551d91842a4aa24e9381c96e143312ac8
leo10099 <leandrohanc@gmail.com>
2018-07-16 15:05:59 -0300

    contact done

--- client/src/components/presentational/Contact/index.js
+++ client/src/components/presentational/Contact/index.js
@@ -11,7 +11,7 @@ export default class Contact extends Component {
         <div className='contact-container'>
           <img src={ContactHeaderImg} className='contact__img-title' alt='' />
           <p className='contact__title'>
-            Esa idea que tenés para que tu Candy sea único nosotras la hacemos realidad con mucha dedicación, atención al detalle, calidad, y mucho
+            Esa idea que tenés para que tu Candy sea único nosotras la hacemos realidad con dedicación, atención al detalle, calidad, y mucho
             {' '} <i className='fa fa-heart texto-rosa' />
           </p>
           <SocialContainer />
